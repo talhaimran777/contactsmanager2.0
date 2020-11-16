1fd042de6ebf1d6b9e9113546935533eea95c9d4
Talha Imran <talhaimran284@gmail.com>
2020-11-16 23:58:23 +0500

    Created database on firestore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import {Provider} from 'react-redux';
+import {ReactReduxFirebaseProvider} from 'react-redux-firebase';
+
+import {store} from './store/store';
+import {rrfProps} from './store/store';
 
 function app() {
     return (
-        <div>
-            <h1>Hello From Redux</h1>
-        </div>
+
+        <Provider store={store}>
+            <ReactReduxFirebaseProvider {...rrfProps}>
+                <div>
+                    <h1>Hello From Redux</h1>
+                </div>
+            </ReactReduxFirebaseProvider>
+        </Provider>
     )
 }
 
