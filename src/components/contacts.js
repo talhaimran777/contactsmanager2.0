d8c0d4fc8cc03b32b5405e282d30ac9f317fbd18
Talha Imran <talhaimran284@gmail.com>
2020-11-19 19:45:26 +0500

    Added a functionality to add a contact using a button on screen when there are no contacts

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import {compose} from 'redux';
 import {connect} from 'react-redux';
-import {firestoreConnect, isLoaded} from 'react-redux-firebase';
+import {Link} from 'react-router-dom';
+import {firestoreConnect, isLoaded, isEmpty} from 'react-redux-firebase';
 
 import Contact from './contact';
 export class contacts extends Component {
     render() {       
         const {contacts} = this.props;
+
         if(!isLoaded(contacts)){
             return (
                 <div className="text-center py-5 my-5">
                     <div className="spinner-border" role="status">
                         <span className="sr-only">Loading...</span>
                     </div>
+                </div>
+            );
+        }
+
+        if(isEmpty(contacts)){
+            return(
+                <div className="">
+                    <h4 className = "text-secondary font-weight-normal">No contacts found!</h4>
+                    <p>Try adding some contacts in your contact list!</p>
+                    <Link to = "contact/add" className="btn btn-success btn-sm">Add Contact</Link>
                 </div>
             );
         }
